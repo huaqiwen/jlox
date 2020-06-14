f7955d14d291200e1a2e47a754a5c6cf064dca4e
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-06-14 10:11:47 -0700

    Add parser. (Chapter 6)

--- src/com/interpreters/lox/Lox.java
+++ src/com/interpreters/lox/Lox.java
@@ -36,7 +36,7 @@ public class Lox {
         BufferedReader reader = new BufferedReader(input);
 
         for (;;) {
-            System.out.print("> ");
+            System.out.print(">>> ");
             run(reader.readLine());
 
             // Reset the flag; one mistake from the user shouldn't kill the entire session
@@ -48,9 +48,13 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression =parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -62,5 +66,13 @@ public class Lox {
                 "[line " + line + "] Error" + where + ": " + message
         );
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
